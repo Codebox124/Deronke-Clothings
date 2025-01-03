b64a4b68451abe9ebc9590199d88f6742e4e7bef
Oyedeji Uzaymah Adeyemi <oyedejiuzaymah@gmail.com>
2025-01-03 11:37:45 +0100

    animations

--- app/components/FollowUs.tsx
+++ app/components/FollowUs.tsx
@@ -1,12 +1,29 @@
+'use client';
 import React from 'react'
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
 import Image from 'next/image'
-
+import { motion } from 'framer-motion';
 export default function FollowUs() {
     return (
         <div className='  mx-auto w-full md:px-8 px-4 mt-7  bg-white text-black'>
             <div className='flex justify-between mb-10  bg-white'>
-                <p className=' text-3xl border-l-4 pl-5'>FOLLOW US ON INSTAGRAM</p>
+                <motion.p
+                initial={{
+                    x:50,
+                    opacity:0
+                  }}
+                  whileInView={{
+                    x:0,
+                    opacity:1
+                  }}
+                  transition={{
+                    duration:0.7,
+                    ease:"easeInOut"
+                  }}
+                  viewport={{
+                    once:true
+                  }}
+                className=' text-3xl border-l-4 pl-5'>FOLLOW US ON INSTAGRAM</motion.p>
 
             </div>
 
@@ -14,19 +31,79 @@ export default function FollowUs() {
 
             <div className='bg-[#FAF3F2]'>
                 <div className=' text-center py-28 px-5 md:px-0'>
-                    <div className='text-[#84635C] flex justify-center'>
+                    <motion.div
+                    initial={{
+            y:50,
+            opacity:0
+          }}
+          whileInView={{
+            y:0,
+            opacity:1
+          }}
+          transition={{
+            duration:0.7,
+            ease:"easeInOut"
+          }}
+          viewport={{
+            once:true
+          }} className='text-[#84635C] flex justify-center'>
                         <FaInstagram size={24} />
-                    </div>
-                    <div className=' text-[#84635C] text-[48px] md:text-[60px] lg:text-[80px] italic my-3'>
+                    </motion.div>
+                    <motion.div
+                    initial={{
+            y:50,
+            opacity:0
+          }}
+          whileInView={{
+            y:0,
+            opacity:1
+          }}
+          transition={{
+            duration:0.7,
+            ease:"easeInOut"
+          }}
+          viewport={{
+            once:true
+          }} className=' text-[#84635C] text-[48px] md:text-[60px] lg:text-[80px] italic my-3'>
                         FOLLOW US
-                    </div>
-                    <div className='capitalize text-[#84635C] mb-3 text-3xl'>
+                    </motion.div>
+                    <motion.div
+                    initial={{
+            y:50,
+            opacity:0
+          }}
+          whileInView={{
+            y:0,
+            opacity:1
+          }}
+          transition={{
+            duration:0.7,
+            ease:"easeInOut"
+          }}
+          viewport={{
+            once:true
+          }} className='capitalize text-[#84635C] mb-3 text-3xl'>
                         <p>ON INSTAGRAM AND OTHER SOCIAL NETWORK</p>
-                    </div>
+                    </motion.div>
 
                     <div className="flex flex-col md:flex-row gap-3 justify-center transition-all">
                     
-                        <a
+                        <motion.a
+                        initial={{
+            y:50,
+            opacity:0
+          }}
+          whileInView={{
+            y:0,
+            opacity:1
+          }}
+          transition={{
+            duration:0.7,
+            ease:"easeInOut"
+          }}
+          viewport={{
+            once:true
+          }}
                             href="https://www.instagram.com/deronkeclothings?igsh=MWpib3c3OTdkbW12YQ=="
                             target="_blank"
                             rel="noopener noreferrer"
@@ -34,10 +111,25 @@ export default function FollowUs() {
                         >
                             <FaInstagram className="text-lg" />
                             INSTAGRAM
-                        </a>
+                        </motion.a>
 
                       
-                        <a
+                        <motion.a
+                        initial={{
+            y:50,
+            opacity:0
+          }}
+          whileInView={{
+            y:0,
+            opacity:1
+          }}
+          transition={{
+            duration:0.7,
+            ease:"easeInOut"
+          }}
+          viewport={{
+            once:true
+          }}
                             href="https://wa.me/447763238511"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -45,7 +137,7 @@ export default function FollowUs() {
                         >
                             <FaWhatsapp className="text-lg" />
                             WHATSAPP
-                        </a>
+                        </motion.a>
                     </div>
                 </div>
             </div>
